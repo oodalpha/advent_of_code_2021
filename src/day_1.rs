7c2e7295cf538e9cbbe0a18c111d79545e7a7482
Amy <amy@amy>
2021-12-04 21:26:01 +0000

    day 1 part 1 complete

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,3 +1,17 @@
+use std::fs;
+
 pub fn get_answer() {
-    println!("Hello, world!");
+    let inputs = fs::read_to_string("./inputs/day_1.txt")
+        .expect("Something went wrong reading the file");
+    let inputs : Vec<i32> = inputs.lines().map(|x| {x.parse().unwrap()}).collect();
+    let mut input_increase_count = 0;
+    let mut previous_value = inputs[0];
+    for index in 1..inputs.len() {
+        let current_value = inputs[index];
+        if current_value > previous_value {
+            input_increase_count += 1;
+        }
+        previous_value = current_value;
+    }
+    println!("The answer is: {}", input_increase_count);
 }
