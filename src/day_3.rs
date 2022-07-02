cc46f0924f801e8932134f05668b003a498cdbe1
Amy <amy>
2022-07-02 19:56:32 +0100

    day 3 part 2 complete

--- src/day_3.rs
+++ src/day_3.rs
@@ -51,6 +51,75 @@ fn part1(inputs: &Vec<Vec<bool>>) {
 }
 
 fn part2(inputs: &Vec<Vec<bool>>) {
-    let answer = 0;
-    println!("The answer to day 2 part 2 is: {}", answer);
+    let mut working_data = inputs.to_vec();
+    for i in 0..inputs[0].len() { //assume first entry is same length as all the others
+        if working_data.len()==1
+        {
+            break;
+        }
+        let majority_count = working_data.len() as f32/2.0;
+        let mut number_of_ones = 0;
+        for j in 0..working_data.len() {
+            let bit = working_data[j][i];
+            match bit {
+                true => number_of_ones += 1,
+                _ => (),
+            }
+        }
+        let bit_to_keep = number_of_ones as f32 >= majority_count;
+        let mut data_copy = Vec::with_capacity(working_data.len());
+        for j in 0..working_data.len() {
+            let bit = working_data [j][i];
+            if bit == bit_to_keep {
+                data_copy.push(working_data[j].to_vec());
+            }
+        }
+        working_data = data_copy;
+    }
+    let oxygen_generator_bits = &working_data[0];
+    let mut oxygen_generator_rating = 0;
+    for i in 0..oxygen_generator_bits.len() {
+        match oxygen_generator_bits[i] {
+            true => oxygen_generator_rating += 1 << oxygen_generator_bits.len()-1-i,
+            _ => ()
+        }
+    }
+
+    //now find co2_scrubber
+    let mut working_data = inputs.to_vec();
+    for i in 0..inputs[0].len() { //assume first entry is same length as all the others
+        if working_data.len()==1
+        {
+            break;
+        }
+        let majority_count = working_data.len() as f32/2.0;
+        let mut number_of_ones = 0;
+        for j in 0..working_data.len() {
+            let bit = working_data[j][i];
+            match bit {
+                true => number_of_ones += 1,
+                _ => (),
+            }
+        }
+        let bit_to_keep = (number_of_ones as f32) < majority_count;
+        let mut data_copy = Vec::with_capacity(working_data.len());
+        for j in 0..working_data.len() {
+            let bit = working_data [j][i];
+            if bit == bit_to_keep {
+                data_copy.push(working_data[j].to_vec());
+            }
+        }
+        working_data = data_copy;
+    }
+    let co2_scrubber_bits = &working_data[0];
+    let mut co2_scrubber_rating = 0;
+    for i in 0..co2_scrubber_bits.len() {
+        match co2_scrubber_bits[i] {
+            true => co2_scrubber_rating += 1 << co2_scrubber_bits.len()-1-i,
+            _ => ()
+        }
+    }
+    println!("most common: {:b}({}), least common: {:b}({}).", oxygen_generator_rating, oxygen_generator_rating, co2_scrubber_rating, co2_scrubber_rating);
+    let answer = oxygen_generator_rating * co2_scrubber_rating;
+    println!("The answer to day 3 part 2 is: {}", answer);
 }
